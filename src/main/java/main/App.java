dae60f6dc307ff154c299e53bc3e43b4bc8a76e1
juusopikapay <juuso@pikapay.fi>
2024-01-22 19:33:40 +0200

    Initial commit

---
+++ src/main/java/main/App.java
@@ -0,0 +1,43 @@
+package main;
+
+import java.util.Scanner;
+
+class App {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Hedgehog siili = new Hedgehog();
+
+        int valinta;
+        
+        do {
+            System.out.println("1) Pistä siili puhumaan, 2) Luo uusi siili, 3) Juoksuta siiliä, 0) Lopeta ohjelma");
+            valinta = scanner.nextInt();
+
+            if (valinta == 1) {
+                    System.out.println("Mitä siili sanoo:");
+                    scanner.nextLine();
+                    String puhe = scanner.nextLine();
+                    siili.puhu(puhe);
+            } else if (valinta == 2) {
+                    System.out.println("Anna siilin nimi:");
+                    String nimi = scanner.next();
+                    System.out.println("Anna siilin ikä:");
+                    int ika = scanner.nextInt();
+                    siili = new Hedgehog(nimi, ika);
+            } else if (valinta == 3) {
+                    System.out.println("Kuinka monta kierrosta?");
+                    int kierrokset = scanner.nextInt();
+                    siili.run(kierrokset);
+            } else if (valinta == 0) {
+                
+                    System.out.println("Kiitos ohjelman käytöstä.");
+            } else {
+
+                    System.out.println("Syöte oli väärä.");
+            }
+
+        } while (valinta != 0);
+
+        scanner.close();
+    }
+}
---
+++ src/main/java/main/Hedgehog.java
@@ -0,0 +1,26 @@
+package main;
+
+class Hedgehog {
+    private String nimi;
+    private int ika;
+
+    public Hedgehog() {
+        this.nimi = "Pikseli";
+        this.ika = 5;
+    }
+
+    public Hedgehog(String nimi, int ika) {
+        this.nimi = nimi;
+        this.ika = ika;
+    }
+
+    public void puhu(String text) {
+        System.out.println(this.nimi + ": " + text);
+    }
+
+    public void run(int kierrokset) {
+        for (int i = 0; i < kierrokset; i++) {
+            System.out.println(this.nimi + " juoksee kovaa vauhtia!");
+        }
+    }
+}
